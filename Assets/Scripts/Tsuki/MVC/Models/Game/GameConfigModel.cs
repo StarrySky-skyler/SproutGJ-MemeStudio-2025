b105ec437e276d43338df8913f93abd16a117f2e
绘星tsuki <xiaoyuesun915@gmail.com>
2025-01-28 23:07:09 +0800

    [add] 游戏配置初始化
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

---
+++ Assets/Scripts/Tsuki/MVC/Models/Game/GameConfigModel.cs
@@ -0,0 +1,18 @@
+﻿// ********************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/01/28 23:01
+// @version: 1.0
+// @description:
+// ********************************************************************************
+
+using UnityEngine;
+
+namespace Tsuki.MVC.Models.Game
+{
+    [CreateAssetMenu(fileName = "GameConfig", menuName = "Tsuki/New Game Config", order = 1)]
+    public class GameConfigModel : ScriptableObject
+    {
+        
+    }
+}
