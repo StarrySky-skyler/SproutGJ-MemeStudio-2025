cc94dd50f58473d5787d9a812ce0059ade27afa0
绘星tsuki <xiaoyuesun915@gmail.com>
2025-02-01 17:25:47 +0800

    [feat] 第一关，bgm随机自动播放
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

---
+++ Assets/Scripts/Tsuki/Managers/AudioFade.cs
@@ -0,0 +1,55 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/02/01 17:02
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using System;
+using DG.Tweening;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Tsuki.Managers
+{
+    public class AudioFade
+    {
+        private readonly AudioManager _audioManager;
+
+        public AudioFade(AudioManager audioManager)
+        {
+            _audioManager = AudioManager.Instance;
+        }
+
+        /// <summary>
+        /// 淡入
+        /// </summary>
+        /// <param name="audioSource"></param>
+        /// <param name="onCompleted"></param>
+        public void FadeIn(AudioSource audioSource,
+            [CanBeNull] Action onCompleted = null)
+        {
+            audioSource.Play();
+            audioSource.DOFade(1, _audioManager.fadeInTime).OnComplete(() =>
+            {
+                onCompleted?.Invoke();
+            });
+        }
+
+        /// <summary>
+        /// 淡出
+        /// </summary>
+        /// <param name="audioSource"></param>
+        /// <param name="onCompleted"></param>
+        public void FadeOut(AudioSource audioSource,
+            [CanBeNull] Action onCompleted = null)
+        {
+            audioSource.DOFade(0, _audioManager.fadeOutTime).OnComplete(() =>
+            {
+                audioSource.Stop();
+                onCompleted?.Invoke();
+            });
+        }
+    }
+}
--- Assets/Scripts/Tsuki/Managers/AudioManager.cs
+++ Assets/Scripts/Tsuki/Managers/AudioManager.cs
@@ -7,39 +7,60 @@
 // *****************************************************************************
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using JetBrains.Annotations;
 using Tsuki.MVC.Models.Player;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Tsuki.Managers
 {
+    /// <summary>
+    /// 音效类型
+    /// </summary>
+    public enum SoundEffectType
+    {
+        Move = 1,
+        Win,
+        Fail
+    }
+
     public class AudioManager : Singleton<AudioManager>
     {
         [Header("音频组件")] public AudioSource bgmAudioSource;
         public AudioSource soundEffectAudioSource;
 
-        [Header("Bgm")] public List<AudioClip> bgmList;
-
-        [Header("音效")] public List<AudioClip> soundEffectList;
-
         [Header("渐入渐出时间")] public float fadeInTime;
         public float fadeOutTime;
 
+        [Header("移动音效")] public List<AudioClip> moveSoundEffectList;
+
+        [Header("胜利音效")] public AudioClip winSoundEffect;
+
+        [Header("失败音效")] public AudioClip failSoundEffect;
+
+        [Header("BGM")] public List<AudioClip> bgmList;
+
         private PlayerModel _playerModel;
-        private bool _moveSoundSwitch;
+        [CanBeNull] private AudioClip _lastMoveSoundEffect;
+        private AudioFade _audioFade;
 
         protected override void Awake()
         {
             base.Awake();
             _playerModel = Resources.Load<PlayerModel>("Tsuki/PlayerModel");
+            _audioFade = new AudioFade(this);
         }
 
         private void Start()
         {
-            bgmAudioSource.loop = true;
+            bgmAudioSource.loop = false;
             soundEffectAudioSource.loop = false;
+            _lastMoveSoundEffect = null;
+            bgmAudioSource.volume = 0;
+            StartCoroutine(PlayBgm());
         }
 
         private void OnEnable()
@@ -55,78 +76,99 @@ namespace Tsuki.Managers
             _playerModel.OnMoveStatusChanged -= PlayMoveSoundEffect;
             BoxManager.Instance.OnWinChanged -= PlayWinSoundEffect;
         }
-
-        private void PlayMoveSoundEffect(bool moveStatus)
+        
+        private IEnumerator PlayBgm()
         {
-            if (!moveStatus) return;
-            PlaySoundEffect(_moveSoundSwitch ? "Move a cat" : "Move a cat2");
-            _moveSoundSwitch = !_moveSoundSwitch;
-        }
-
-        private void PlayWinSoundEffect(bool winStatus)
-        {
-            PlaySoundEffect(winStatus ? "Victroy this pat" : "Fail");
+            while (true)
+            {
+                RandomPlayBgm();
+                yield return new WaitForSeconds(bgmAudioSource.clip.length - fadeOutTime);
+                _audioFade.FadeOut(bgmAudioSource);
+                yield return new WaitForSeconds(fadeOutTime);
+            }
         }
 
         /// <summary>
-        /// 播放Bgm
+        /// 播放移动音效
         /// </summary>
-        /// <param name="bgmName"></param>
-        public void PlayBgm(string bgmName)
+        /// <param name="moveStatus"></param>
+        private void PlayMoveSoundEffect(bool moveStatus)
         {
-            // 正在播放，渐出后渐入
-            if (bgmAudioSource.isPlaying)
-            {
-                FadeOut(bgmAudioSource, () =>
-                {
-                    SetBgm(bgmName);
-                    FadeIn(bgmAudioSource);
-                });
-            }
-            // 未播放，直接渐入
-            else
-            {
-                SetBgm(bgmName);
-                FadeIn(bgmAudioSource);
-            }
+            if (!moveStatus) return;
+            PlaySoundEffect(SoundEffectType.Move);
+        }
+
+        /// <summary>
+        /// 播放胜利或失败音效
+        /// </summary>
+        /// <param name="winStatus"></param>
+        private void PlayWinSoundEffect(bool winStatus)
+        {
+            PlaySoundEffect(winStatus
+                ? SoundEffectType.Win
+                : SoundEffectType.Fail);
+        }
+
+        /// <summary>
+        /// 随机循环播放Bgm
+        /// </summary>
+        public void RandomPlayBgm()
+        {
+            SetRandomBgm();
+            _audioFade.FadeIn(bgmAudioSource);
         }
 
         /// <summary>
         /// 播放一次音效
         /// </summary>
-        /// <param name="soundEffectName"></param>
-        public void PlaySoundEffect(string soundEffectName)
+        /// <param name="soundEffectType"></param>
+        public void PlaySoundEffect(SoundEffectType soundEffectType)
         {
-            AudioClip clip =
-                Resources.Load<AudioClip>("Tsuki/AudioClips/SoundEffect/" +
-                                          soundEffectName);
+            switch (soundEffectType)
+            {
+                case SoundEffectType.Move:
+                    // 第一次播放移动音效
+                    AudioClip clip = null;
+                    if (!_lastMoveSoundEffect)
+                    {
+                        clip = moveSoundEffectList[Random.Range(0, moveSoundEffectList.Count - 1)];
+                    }
+                    // 移动音效与上次不同，播放新的移动音效
+                    else
+                    {
+                        clip = _lastMoveSoundEffect;
+                        while (clip == _lastMoveSoundEffect)
+                        {
+                            clip = moveSoundEffectList[Random.Range(0, moveSoundEffectList.Count - 1)];
+                        }
+                    }
 
-            soundEffectAudioSource.PlayOneShot(clip);
+                    soundEffectAudioSource.PlayOneShot(clip);
+                    _lastMoveSoundEffect = clip;
+
+                    break;
+                case SoundEffectType.Win:
+                    soundEffectAudioSource.PlayOneShot(winSoundEffect);
+                    break;
+                case SoundEffectType.Fail:
+                    soundEffectAudioSource.PlayOneShot(failSoundEffect);
+                    break;
+                default:
+                    Debug.LogError($"{soundEffectType.ToString()}音效类型不存在");
+                    break;
+            }
         }
 
-        private void SetBgm(string bgmName)
+        private void SetRandomBgm()
         {
-            AudioClip clip =
-                Resources.Load<AudioClip>("Tsuki/AudioClips/Bgm/" + bgmName);
+            AudioClip lastClip = bgmAudioSource.clip;
+            AudioClip clip = lastClip;
+            while (clip == lastClip)
+            {
+                clip = bgmList[Random.Range(0, bgmList.Count - 1)];
+            }
+
             bgmAudioSource.clip = clip;
-        }
-
-        private void FadeIn(AudioSource audioSource,
-            [CanBeNull] Action onCompleted = null)
-        {
-            audioSource.DOFade(1, fadeInTime).OnComplete(() =>
-            {
-                onCompleted?.Invoke();
-            });
-        }
-
-        private void FadeOut(AudioSource audioSource,
-            [CanBeNull] Action onCompleted = null)
-        {
-            audioSource.DOFade(0, fadeOutTime).OnComplete(() =>
-            {
-                onCompleted?.Invoke();
-            });
         }
     }
 }
