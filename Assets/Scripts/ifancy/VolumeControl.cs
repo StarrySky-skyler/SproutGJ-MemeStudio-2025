bf42dbf2fea8382c052f20b54c57c90499182756
绘星tsuki <xiaoyuesun915@gmail.com>
2025-02-09 12:53:00 +0800

    [fix] 修复音效关联问题
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

--- Assets/Scripts/ifancy/VolumeControl.cs
+++ Assets/Scripts/ifancy/VolumeControl.cs
@@ -4,27 +4,36 @@ using UnityEngine.UI;
 public class VolumeControl : MonoBehaviour
 {
     public Slider volumeSlider; // ����������
-    public AudioSource audioSource; // ��ƵԴ�����ڿ�������
+    private AudioSource _audioSource; // ��ƵԴ�����ڿ�������
 
     private void Start()
     {
+        if (gameObject.name.Contains("音乐"))
+            _audioSource = GameObject.FindWithTag("Audio")
+                .GetComponents<AudioSource>()[0];
+        else
+        {
+            _audioSource = GameObject.FindWithTag("Audio")
+                .GetComponents<AudioSource>()[1];
+        }
+
         // ��ʼ��ʱ���û�������ֵΪ��ǰ��ƵԴ������
         //volumeSlider.value = audioSource.volume;
-        switch (audioSource.outputAudioMixerGroup.name)
+        switch (_audioSource.outputAudioMixerGroup.name)
         {
             case "BGM":
-                audioSource.outputAudioMixerGroup.audioMixer.GetFloat(
+                _audioSource.outputAudioMixerGroup.audioMixer.GetFloat(
                     "BGMVolume", out float bgmVolume);
                 volumeSlider.value = Mathf.Pow(10, bgmVolume / 20);
                 break;
             case "SoundEffect":
-                audioSource.outputAudioMixerGroup.audioMixer.GetFloat(
+                _audioSource.outputAudioMixerGroup.audioMixer.GetFloat(
                     "SFXVolume", out float soundEffectVolume);
                 volumeSlider.value = Mathf.Pow(10, soundEffectVolume / 20);
                 break;
             default:
                 Debug.LogWarning("Unknown audio mixer group: " +
-                                 audioSource.outputAudioMixerGroup.name);
+                                 _audioSource.outputAudioMixerGroup.name);
                 break;
         }
 
@@ -35,19 +44,19 @@ public class VolumeControl : MonoBehaviour
     // ��������ֵ�ı�ʱ����
     private void OnVolumeChanged(float value)
     {
-        switch (audioSource.outputAudioMixerGroup.name)
+        switch (_audioSource.outputAudioMixerGroup.name)
         {
             case "BGM":
-                audioSource.outputAudioMixerGroup.audioMixer.SetFloat(
+                _audioSource.outputAudioMixerGroup.audioMixer.SetFloat(
                     "BGMVolume", Mathf.Clamp(Mathf.Log10(value) * 20, -80, 0));
                 break;
             case "SoundEffect":
-                audioSource.outputAudioMixerGroup.audioMixer.SetFloat(
+                _audioSource.outputAudioMixerGroup.audioMixer.SetFloat(
                     "SFXVolume", Mathf.Clamp(Mathf.Log10(value) * 20, -80, 0));
                 break;
             default:
                 Debug.LogWarning("Unknown audio mixer group: " +
-                                 audioSource.outputAudioMixerGroup.name);
+                                 _audioSource.outputAudioMixerGroup.name);
                 break;
         }
         //audioSource.volume = value;  // ������ƵԴ������
