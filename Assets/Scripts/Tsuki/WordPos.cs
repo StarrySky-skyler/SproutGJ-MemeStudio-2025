8a3bb531856fbbbe36145ca7c543c716bc8ff749
绘星tsuki <xiaoyuesun915@gmail.com>
2025-02-09 22:41:54 +0800

    [feat] 开发者寄语
    Signed-off-by: 绘星tsuki <xiaoyuesun915@gmail.com>

---
+++ Assets/Scripts/Tsuki/WordPos.cs
@@ -0,0 +1,24 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/02/09 22:02
+// @version: 1.0
+// @description:
+// *****************************************************************************
+
+using System;
+using UnityEngine;
+
+namespace Tsuki
+{
+    public class WordPos : MonoBehaviour
+    {
+        public GameObject word;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
+            word.SetActive(true);
+        }
+    }
+}
